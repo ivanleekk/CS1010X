9d54dc513fd280126cc9c12600320421c5924de0
Ivan <84584280+aRandombo@users.noreply.github.com>
2022-04-20 11:58:35 +0800

    complete mission 11b

--- Mission 11B/mission11b-template.py
+++ Mission 11B/mission11b-template.py
@@ -6,7 +6,7 @@
 # Note that written answers are commented out to allow us to run your
 # code easily while grading your problem set.
 
-# Collaborator: <your collaborator's matric> Francis Chu
+# Collaborator: A0248890N Francis Chu
 
 ###############
 # Mission 11b #
@@ -17,11 +17,12 @@
 ##########
 
 # With these operations, compound generic operations can be defined, such as
-# def square(x):
-#   return mul(x, x)
+def square(x):
+    return mul(x, x)
+
 
 # (a) What are the types of the input and output of the generic square operation?
-# Answer: they are both ordinary numbers
+# Answer: they are both generic numbers
 
 # (b) Why would we prefer to define square in the above way, rather than:
 # def square(x):
@@ -42,7 +43,17 @@
 # such as create_ordinary, and the operations we can apply on Generic-Num, such
 # as add. How is make_ord invoked, and how is add_ord invoked?
 
-# Answer:
+# Answer: when using an operator such as add or negate, you are working with a generic number that already
+# has a tag applied on to it and the tuple comes from reading the tag of each number in the arguments,
+# in the case of add(), 2 arguments which gives it ("ordinary", "ordinary") or in the case of negate(),
+# 1 argument which gives it ("ordinary"). using these tags, apply generic uses a series of if statements
+# to find which function to return and apply on the arguments, e.g. add_ord() or negate_ord() and use it
+# on the given arguments in generic add or generic negate.
+
+# However, for create_ordinary(), the input is not a generic number but a raw python number like an int or float.
+# these numbers do not have tags and rather the tag with string "ordinary" comes from
+# create_ordinary() which adds "ordinary" as the index to search for make_ord inside the make functions
+# in order to apply it on to the raw number input from create_ordinary() to make a generic ordniary number
 
 ##########
 # Task 3 #
@@ -57,9 +68,15 @@
 # What happens when you use the wrong way to produce 9+10i and 3+10i and then try to add
 # them? Why does this happen?
 
-# Right way:
-# What happens:
-# Why it happens:
+# Right way: second way
+
+# What happens: when you use the first way and try to add the numbers, you will get a "bad tagged datum" error
+
+# Why it happens: This happens because in the complex number package, the basic operators such as "add",
+# "sub", "mul" etc all call their generic counterparts inside the function to do the function such as adding
+#  the real parts and imag parts together. But to call a generic add, you have to use a generic number, which
+# is why using the first way will not work as the numbers are raw integers and not tagged, causing the generic
+# operators to fail and give a "bad tagged datum error"
 
 ##########
 # Task 4 #
@@ -83,8 +100,9 @@
 #         "ordinary"  5
 
 # FILL IN YOUR ANSWERS HERE:
-# c2_plus_7i =
-# c3_plus_1i =
+# c2_plus_7i = create_complex(create_ordinary(2), create_ordinary(7))
+# c3_plus_1i = create_complex(create_ordinary(3), create_ordinary(1))
+
 
 # csq = square(sub(c2_plus_7i, c3_plus_1i))
 
@@ -109,7 +127,8 @@
 # handled the addition operation. Why is it not
 # possible to name this function "add"?
 
-# Answer:
+# Answer: if you name it "add", it will overwrite the generic_add function, which will prevent the add
+# function from working for the other data types such as ordinary or rational numbers.
 
 ##########
 # Task 6 #
@@ -158,27 +177,30 @@ def install_complex_package():
 
     def complex_conjugate(x):
         return make_com(real(x), negate(imag(x)))
-    # the 3 functions below are also by me
-    def negate_com(x): #(rep-com) -> generic-com
+
+    # the 3 functions below are by me
+    def negate_com(x):  # (rep-com) -> generic-com
         return make_com(negate(real(x)), negate(imag(x)))
-    
-    def is_zero_com(x): # (rep-com) -> Boolean
+
+    def is_zero_com(x):  # (rep-com) -> Boolean
         if real(x) == 0 and imag(x) == 0:
             return True
         else:
             return False
-    def is_eq_com(x,y):# (rep-com,rep-com) -> Boolean
+
+    def is_eq_com(x, y):  # (rep-com,rep-com) -> Boolean
         return x == y
-        
+
     put("make", "complex", make_com)
     put("add", ("complex", "complex"), add_com)
     put("sub", ("complex", "complex"), sub_com)
     put("mul", ("complex", "complex"), mul_com)
     put("div", ("complex", "complex"), div_com)
-
+    # added the 3 put statements below to allow generic functions to access these
     put("negate", ("complex",), negate_com)
     put("is_zero", ("complex",), is_zero_com)
     put("is_equal", ("complex", "complex"), is_eq_com)
+
 
 install_complex_package()
 
@@ -189,8 +211,10 @@ def create_complex(x, y):
 
 # Change the values for the test variables below
 c_neg3_plus_10i = create_complex(create_ordinary(-3), create_ordinary(10))
-c1_plus_2i = create_complex(create_ordinary(1),create_ordinary(2))
+c1_plus_2i = create_complex(create_ordinary(1), create_ordinary(2))
 c1_plus_3i = create_complex(create_ordinary(1), create_ordinary(3))
+# com1 = create_complex(1, 2)
+# print(add(com1, com1))
 print(c_neg3_plus_10i)
 print(negate(c_neg3_plus_10i))
 #################
@@ -206,3 +230,7 @@ def gradeThis():
 
 
 gradeThis()
+
+# c1 = create_complex(create_ordinary(2), create_ordinary(7))
+# c2 = create_complex(create_ordinary(3), create_ordinary(1))
+# print(square(sub(c1, c2)))
